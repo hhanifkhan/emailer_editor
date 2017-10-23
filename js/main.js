3ed5328036a18d5c48b0e804d2263901c6723f1b
hhanifkhan <hhanif.khan.786@gmail.com>
2017-10-23 15:09:46 +0530

    before copy and delete filter2

--- js/main.js
+++ js/main.js
@@ -47,7 +47,7 @@ const gridTemplatesAll = {
 	</table>
 	`
 };
-
+//test
 //grid template end
 const componentAll={
 	"ck_text" : `<p class="editor" ondblclick="makeEditable(event,this)" data-ce="1" class="ve_text" style="    border: 1px solid green;">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>`,
